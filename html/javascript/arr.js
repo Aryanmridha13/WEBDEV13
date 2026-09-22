61e2d3376664982311c9bd94c2d4c937a2eaadb3
Aryan Mridha <aryanmridha8@gmail.com>
2026-09-23 00:44:32 +0530

    operators in java script

--- html/javascript/arr.js
+++ html/javascript/arr.js
@@ -3,19 +3,19 @@ let movies = ['KGF','KANTARA','SPIDERMAN','BAHUBALI','PUSHPA','MIRZAPUR','GOT']
 console.log(movies);
 
 // length of array
-console.log(movies.length);
+// console.log(movies.length);
 
 // add the element 
 movies[7] ='don'
 
 // update 
 movies[0]='don'
-console.log(movies);
+// console.log(movies);
 
 // delete
 delete movies[7]
-console.log(movies);
-console.log(movies[7]);
+// console.log(movies);
+// console.log(movies[7]);
 
 let details = {
     Name : "harsh",
@@ -25,6 +25,18 @@ let details = {
     isActive:true
 }
 
-console.log(details.Name);
-console.log(details.des);
-console.log(details.cont);
+// console.log(details.Name);
+// console.log(details.des);
+// console.log(details.cont);
+
+
+let arr1 = [10,20,'name',true,'day']
+console.log(arr1);
+
+console.log(arr1[2]);
+console.log(arr1[8]);
+console.log(arr1[3]);
+console.log(arr1[1]);
+
+
+
---
+++ html/javascript/operator.js
@@ -0,0 +1,26 @@
+// arithematic operator
+let a = 10
+let b = 20
+
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+
+
+let x = 5
+console.log(--x);
+console.log(x++);
+console.log(++x);
+console.log(x--);
+console.log(++x);
+console.log(x++);
+console.log(x++);
+console.log(--x);
+console.log(++x);
+console.log(x);
+
+
