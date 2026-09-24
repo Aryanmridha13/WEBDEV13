5f783368b82cc0046275004aaa57f85f3586ef06
Aryan Mridha <aryanmridha8@gmail.com>
2026-09-25 01:08:34 +0530

    operators

---
+++ html/javascript/assigope.js
@@ -0,0 +1,46 @@
+// =
+let x = 20
+console.log(x);
+
+// =+
+
+x+=5
+console.log(x);
+
+// -=
+x-=3
+console.log(x);
+// /=
+x/=4
+console.log(x);
+
+// %=
+
+let y = 20
+
+x%=5
+y%=5
+console.log(x,y);
+
+// *=
+
+console.log(x*=2);
+
+// **=
+
+let z = 5 
+z**=2
+console.log(z);
+
+// &&=
+
+let a = 3
+
+console.log(a&&=2);
+
+
+
+
+
+
+
