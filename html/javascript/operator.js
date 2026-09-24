1d71f53a5b6ec132b6ad5ad0bcdc9d142c33e234
Aryan Mridha <aryanmridha8@gmail.com>
2026-09-24 19:39:56 +0530

    operator in javascript

--- html/javascript/operator.js
+++ html/javascript/operator.js
@@ -1,6 +1,6 @@
 // arithematic operator
-let a = 10
-let b = 20
+// let a = 10
+// let b = 20
 
 
 // console.log(a + b);
@@ -11,16 +11,62 @@ let b = 20
 // console.log(a ** b);
 
 
-let x = 5
-console.log(--x);
-console.log(x++);
-console.log(++x);
-console.log(x--);
-console.log(++x);
-console.log(x++);
-console.log(x++);
-console.log(--x);
-console.log(++x);
-console.log(x);
+// let x = 5
+// console.log(--x);
+// console.log(x++);
+// console.log(++x);
+// console.log(x--);
+// console.log(++x);
+// console.log(x++);
+// console.log(x++);
+// console.log(--x);
+// console.log(++x);
+// console.log(x);
+
+// comperision operator
+
+let a = 10
+let b = 20 
+let c = '10'
+let d = 15
+
+// console.log(a == c); //! it will only check the value not the data type
+
+// console.log(a === c); //! it will check both value or data type
+
+// console.log(a != c);
+// console.log(a !== c);
+
+
+// console.log(a>b);
+// console.log(a<c);
+
+
+// logical and(&&) logical or (||) logical not (~)
+
+// console.log(a<b && a>c);
+
+// console.log(a<d || a>b );
+
+// console.log((a<=c));
+// console.log(b>=d);
+
+
+// ternary operator 
+//  syntax = (conditon)? "state 1" : "state 2" 
+
+let age = 25
+let res = (age>24)? 'eligible for wedding' : 'not eligible for wedding'
+console.log(res);
+
+// assigment operator
+// => = , +=,-=,*=,/=,%=,**=
+
+
+
+
+
+
+
 
 
